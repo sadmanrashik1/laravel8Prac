075ba74bab02d5e42f8297004b9a3c1679d53a9b
man <man@recoverys-MBP.lan>
2022-01-26 21:38:34 +0600

    ambi route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,34 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//route that sends back a view
 Route::get('/', function () {
     return view('welcome');
+    #return env('CREATOR_NAME');
 });
+
+//Route to users - a string
+Route::get('/users',function (){
+    return 'Welcome to the users page';
+});
+
+//Route to users - Arraay (json)
+Route::get('/users',function (){
+    return ['r1','r2','r3','listend'];
+});
+
+//Route to users - Arraay (json)
+Route::get('/users',function (){
+    return response()->json([
+        'name' =>'laravel',
+        'practise' => 'basics' 
+    ]);
+}); 
+
+//Route to users -function
+Route:: get('/users',function(){
+    return redirect('/');
+});
+
+//practise.com ==/
+//practise.com/users == /users
